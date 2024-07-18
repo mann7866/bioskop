d61b80dcbfd50d4d3266d864414e5573f61780b6
mann7866 <akunkudewe861@gmail.com>
2024-07-18 14:28:44 +0700

    menambah crud kumpulan Genre

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
@@ -11,24 +11,17 @@
     <title>{{ config('app.name', 'Bioskop') }}</title>
 
     <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-<<<<<<< HEAD
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-=======
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- AOS CSS -->
-    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
->>>>>>> 1e438dbea23281908bed31c202b8f6ccb4a93089
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
 <body>
+
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm sticky-top">
             <div class="container">
@@ -48,27 +41,10 @@
                             <a class="nav-link" href="{{ route('genre') }}">{{ __('Genres') }}</a>
                         </li>
                         <li class="nav-item">
-<<<<<<< HEAD
                             <a class="nav-link" href="#">{{ __('Details') }}</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">{{ __('Film') }}</a>
-=======
-                            <a class="nav-link" href="{{ route('detail') }}">{{ __('Details') }}</a>
-                        </li>
-                        <!-- Dropdown Menu -->
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                Fitur
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <li><a class="dropdown-item" href="{{ route('genre') }}">Genres</a></li>
-                                <li><a class="dropdown-item" href="{{ route('time') }}">Time</a></li>
-                                <li><a class="dropdown-item" href="{{ route('detail') }}">Details</a></li>
-                                <li><a class="dropdown-item" href="{{ route('genres') }}">Kumpulan Genre</a></li>
-                            </ul>
->>>>>>> 1e438dbea23281908bed31c202b8f6ccb4a93089
                         </li>
                     </ul>
 
@@ -79,7 +55,6 @@
                         <button class="btn btn-outline-success" type="submit">Search</button>
                     </form>
 
-<<<<<<< HEAD
                     <div class="btn-group">
                         <button class="btn btn-secondary btn-sm dropdown-toggle" type="button"
                             data-bs-toggle="dropdown" aria-expanded="false">
@@ -101,8 +76,6 @@
                         </ul>
                     </div>
 
-=======
->>>>>>> 1e438dbea23281908bed31c202b8f6ccb4a93089
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
@@ -126,14 +99,14 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    {{-- <a class="dropdown-item" href="#">{{ __('Data') }}</a> --}}
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
-                                                      document.getElementById('logout-form').submit();">
+                                                  document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                        class="d-none">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
                                 </div>
@@ -147,24 +120,18 @@
         <main class="py-4">
             @yield('content')
         </main>
+
     </div>
-
-    <!-- SweetAlert -->
+    {{-- sweetalert --}}
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
-    <!-- Ionicons -->
+    {{-- icons --}}
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-
-    <!-- DataTables -->
+    {{-- datatables --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
     <script src="https://cdn.datatables.net/2.0.8/js/dataTables.bootstrap5.js"></script>
-<<<<<<< HEAD
     {{-- cdn bosstraps --}}
-=======
-
-    <!-- Bootstrap JavaScript and Popper.js -->
->>>>>>> 1e438dbea23281908bed31c202b8f6ccb4a93089
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
     </script>
@@ -174,15 +141,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
-<<<<<<< HEAD
-=======
-
-    <!-- AOS JavaScript -->
-    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-    <script>
-        AOS.init();
-    </script>
->>>>>>> 1e438dbea23281908bed31c202b8f6ccb4a93089
 </body>
 
 </html>
