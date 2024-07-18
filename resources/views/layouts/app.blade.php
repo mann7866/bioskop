1e438dbea23281908bed31c202b8f6ccb4a93089
Mardev <rozakadm@gmail.com>
2024-07-18 14:10:23 +0700

    Push Nvabar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,6 @@
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-<<<<<<< HEAD
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -20,26 +19,11 @@
     <!-- AOS CSS -->
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 
-    <!-- Bootstrap JavaScript and Popper.js -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
-    <!-- AOS JavaScript -->
-    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-    <script>
-        AOS.init();
-    </script>
-=======
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
->>>>>>> 34cda4bbcf717cdad8272d7d4abbeccca33fdbef
-
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
 <body>
-
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm sticky-top">
             <div class="container">
@@ -59,10 +43,6 @@
                             <a class="nav-link" href="{{ route('genre') }}">{{ __('Genres') }}</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">{{ __('Details') }}</a>
-                        </li>
-                        <li class="nav-item">
-<<<<<<< HEAD
                             <a class="nav-link" href="{{ route('detail') }}">{{ __('Details') }}</a>
                         </li>
                         <!-- Dropdown Menu -->
@@ -77,9 +57,6 @@
                                 <li><a class="dropdown-item" href="{{ route('detail') }}">Details</a></li>
                                 <li><a class="dropdown-item" href="{{ route('genres') }}">Kumpulan Genre</a></li>
                             </ul>
-=======
-                            <a class="nav-link" href="#">{{ __('Film') }}</a>
->>>>>>> 34cda4bbcf717cdad8272d7d4abbeccca33fdbef
                         </li>
                     </ul>
 
@@ -90,30 +67,6 @@
                         <button class="btn btn-outline-success" type="submit">Search</button>
                     </form>
 
-<<<<<<< HEAD
-=======
-                    <div class="btn-group">
-                        <button class="btn btn-secondary btn-sm dropdown-toggle" type="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Small button
-                        </button>
-                        <ul class="dropdown-menu text-center">
-                            <li>
-                                <a href="{{ route('genre') }}">Genres</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('time') }}">Time</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('detail') }}">Detail</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('genres') }}">genre</a>
-                            </li>
-                        </ul>
-                    </div>
-
->>>>>>> 34cda4bbcf717cdad8272d7d4abbeccca33fdbef
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
@@ -170,12 +123,8 @@
     <!-- DataTables -->
     <script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
     <script src="https://cdn.datatables.net/2.0.8/js/dataTables.bootstrap5.js"></script>
-<<<<<<< HEAD
-{{-- script --}}
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-=======
-    {{-- cdn bosstraps --}}
+
+    <!-- Bootstrap JavaScript and Popper.js -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
     </script>
@@ -185,7 +134,12 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
->>>>>>> 34cda4bbcf717cdad8272d7d4abbeccca33fdbef
+
+    <!-- AOS JavaScript -->
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
 </body>
 
 </html>
